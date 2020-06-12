490a8f2b7015250c9c31662e6b471f849212c7b0
rwilt <rosie.wilt@gmail.com>
2020-06-12 18:44:26 -0400

    file structure

--- config/application.rb
+++ config/application.rb
@@ -34,15 +34,6 @@ module ChangeAgents
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource(
-          '*',
-          headers: :any,
-          methods: [:get, :patch, :put, :delete, :post, :options]
-          )
-      end
-    end
+  
   end
 end
